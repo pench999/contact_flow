e0eb84d0c31d4ded037afe64e80c9381596bbabd
kudo <kudo@kudo-desk.fairway.office>
2025-07-01 15:41:41 +0900

    リスナーポートの修正

--- contact.py
+++ contact.py
@@ -526,5 +526,5 @@ def admin_import():
 
 if __name__ == '__main__':
     init_db()
-    run(app=app, host='0.0.0.0', port=8081, debug=True)
+    run(app=app, host='0.0.0.0', port=8080, debug=True)
 
